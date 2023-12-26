9291aa0545e383116f21483a5fb2b5ef4411c69b
LimDongHyeon <ilhai3@naver.com>
2023-12-26 22:49:23 +0900

    first commit

---
+++ main.js
@@ -0,0 +1,4 @@
+let m = require('./math-tools.js');
+// require : 모듈을 로드해주는 함수
+
+console.log(m.add(1, 2));
---
+++ math-tools.js
@@ -0,0 +1,7 @@
+function add(a, b) {
+    return a + b;
+}
+
+exports.add = add;
+// exports : 내보내다
+// exports.모듈 외부로 공개할 이름 = 모듈 내부에서의 이름
